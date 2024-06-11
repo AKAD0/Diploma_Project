f45b49ade9a294c64fb37348fca6730c847f84e9
Mamma Mia <onixon2010@gmail.com>
2024-06-11 18:27:11 +0800

    modified:   app.py 	modified:   templates/base.html

--- app.py
+++ app.py
@@ -12,22 +12,22 @@ app.app_context().push()        # flask_sqlalchemy troubleshooting
 
 # --- Database
 # setup DB files for the app & create DB object
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_BINDS'] = {'topic_db': 'sqlite:///topic_db.sqlite'}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qna_db.sqlite'
+app.config['SQLALCHEMY_BINDS'] = {'thesis_db': 'sqlite:///thesis_db.sqlite'}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # define DB models //table of 2 columns
-class History( db.Model):
+class QNA( db.Model):
     id = db.Column( db.Integer, primary_key=True)
     message = db.Column( db.String(2000))
-class Topic( db.Model):
-    __bind_key__ = 'topic_db'
-    topic_id = db.Column( db.Integer, primary_key=True)
-    topic_message = db.Column( db.String(2000))
+class Thesis( db.Model):
+    __bind_key__ = 'thesis_db'
+    thesis_id = db.Column( db.Integer, primary_key=True)
+    thesis_message = db.Column( db.String(2000))
 
 # create and init DBs
-db.create_all(bind_key=[None, 'topic_db'])
+db.create_all(bind_key=[None, 'thesis_db'])
 
 
 
@@ -35,23 +35,23 @@ db.create_all(bind_key=[None, 'topic_db'])
 # 1. Homepage endpoint
 @app.get("/")
 def home():
-    history_list = db.session.query(History).all()                    # load the DB
-    return render_template( "base.html", history_list=history_list)   # render html using the loaded DB
+    qna_list = db.session.query(QNA).all()                    # load the DB
+    return render_template( "base.html", qna_list=qna_list)   # render html using the loaded DB
 
 
 # 2. Send button endpoint
 @app.post("/button")                            #} This send button endpoint does 3 things:
                                                 #} 1) Sends prompt & saves response
-                                                #} 2) Adds prompt & response to 'History' DB
-                                                #} 3) Creates another topic for 'Topic' DB
+                                                #} 2) Adds prompt & response to 'QNA' DB
+                                                #} 3) Creates another thesis for 'Thesis' DB
                                                 #} 4) Refreshes page
 def button():
-    # Loading 5 last topics from 'Topic' DB                                                
-    topic = ''
-    topic_temp = db.session.query(Topic).order_by(Topic.topic_id.desc()).limit(5).all()
-    for entry in topic_temp:
-        topic = topic+entry.topic_message+'; '
-    input = topic
+    # Loading 5 last thesises from 'Thesis' DB                                                
+    thesis = ''
+    thesis_temp = db.session.query(Thesis).order_by(Thesis.thesis_id.desc()).limit(5).all()
+    for entry in thesis_temp:
+        thesis = thesis+entry.thesis_message+'; '
+    input = thesis
 
 
     # 1) Send prompt & save response
@@ -69,18 +69,18 @@ def button():
     response = cut[1]
 
 
-    # 2) Add prompt & response to 'History' DB
-    new_history = History( message=prompt)      # declare prompt DB sample
-    db.session.add( new_history)                #} add&commit new sample to DB
+    # 2) Add prompt & response to 'QNA' DB
+    new_qna = QNA( message=prompt)      # declare prompt DB sample
+    db.session.add( new_qna)                #} add&commit new sample to DB
     db.session.commit()                         #}
-    new_history = History( message=response)    # declare prompt DB sample
-    db.session.add( new_history)                #} add&commit new sample to DB
+    new_qna = QNA( message=response)    # declare prompt DB sample
+    db.session.add( new_qna)                #} add&commit new sample to DB
     db.session.commit()                         #}
 
 
-    # 3) Create another topic for 'Topic' DB
+    # 3) Create another thesis for 'Thesis' DB
     prompt = ''
-    prompt_temp = db.session.query(History).order_by(History.id.desc()).limit(2).all()
+    prompt_temp = db.session.query(QNA).order_by(QNA.id.desc()).limit(2).all()
     prompt = 'In less than exactly 5 words explain what was this dialogue about: me: ' + prompt_temp[1].message + ' you: ' + prompt_temp[0].message
     response_json = requests.post(
                             "http://127.0.0.1:8000/predict", 
@@ -93,8 +93,8 @@ def button():
     cut = response_str.split("### Response:")
     response = cut[1]
     
-    new_topic = Topic( topic_message=response)                # declare prompt DB sample
-    db.session.add( new_topic)     #} add&commit new sample to DB
+    new_thesis = Thesis( thesis_message=response)                # declare prompt DB sample
+    db.session.add( new_thesis)     #} add&commit new sample to DB
     db.session.commit()            #}
 
 
@@ -103,10 +103,10 @@ def button():
 
 
 # 3. Delete button endpoint
-@app.get("/delete/<int:history_id>")
-def delete(history_id):
-    history = db.session.query(History).filter(History.id == history_id).first()
-    db.session.delete(history)
+@app.get("/delete/<int:qna_id>")
+def delete(qna_id):
+    qna = db.session.query(QNA).filter(QNA.id == qna_id).first()
+    db.session.delete(qna)
     db.session.commit()
     return redirect( url_for( "home"))
 
